c557a729e7bd11adca89498152c688a2eca2693c
ChaosBelcebub <mich3ael@web.de>
2016-04-04 12:03:03 +0200

    Working on Excercise 3 with open issues

--- excercise2/excercise3/Main.cpp
+++ excercise2/excercise3/Main.cpp
@@ -15,29 +15,65 @@ class TestObjekt
   {
     ++anz;
     count = anz;
-    cout << "Konstrutor Testobjekt " << count << endl;
+    cout << "Konstrutor (" << count << ")" << endl;
   }
 
   TestObjekt(TestObjekt& t)
   {
     ++ anz;
     count = anz;
-    cout << "Kopierkonstruktor TestObjekt " << count << endl;
+    cout << "Kopierkonstruktor (" << t.getCount() << " -> " << count << ")" << endl;
   }
 
   ~TestObjekt()
   {
-    cout << "Destruktor " << count << endl;
+    cout << "Destruktor (" << count << ")" << endl;
+  }
+
+  int getCount()
+  {
+    return count;
   }
 };
 int TestObjekt::anz = 0;
 
+// Tests
+// Global variable
+TestObjekt t1;
 
+// Global function
+void testFunk(TestObjekt to)
+{
+}
+
+void testFunk2(TestObjekt& to)
+{
+}
+
+TestObjekt testFunk()
+{
+  TestObjekt t;
+  return t;
+}
 
 int main(int argc, char *argv[])
 {
-  TestObjekt t1;
+  // Local
+  cout << "b)" << endl;
   TestObjekt t2;
-  TestObjekt t3 = t1;
-  TestObjekt t4 = t2;
+  cout << endl;
+
+  cout << "c)" << endl;
+  testFunk(t1);
+  testFunk(t2);
+  cout << endl;
+
+  cout << "d)" << endl;
+  testFunk2(t1);
+  cout << endl;
+
+  cout << "e)" << endl;
+  TestObjekt t3 = testFunk();
+  testFunk();
+  cout << endl;
 }
