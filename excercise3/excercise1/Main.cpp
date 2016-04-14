e171fa37b552536c3609bd8725c0eed9aeefd1f5
ChaosBelcebub <mich3ael@web.de>
2016-04-14 19:36:23 +0200

    Finished first excercise

---
+++ excercise3/excercise1/Main.cpp
@@ -0,0 +1,43 @@
+// Michael Kotzjan
+// HFU 11.04.2015
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class dyna
+{
+  int *p;
+  public:
+  dyna(int i);
+  ~dyna() { delete p; }
+  int get() { return *p; }
+};
+
+dyna::dyna(int i)
+{
+  p = new int;
+  *p = i;
+}
+
+// gib negativen Wert
+// zurueck
+int neg(dyna &ob)
+{
+  return -1*ob.get();
+}
+
+int main()
+{
+  dyna o(-10);
+  cout << o.get() << "\n";
+  cout << neg(o) << "\n";
+  dyna o2(20);
+  cout << o2.get() << "\n";
+  cout << neg(o2) << "\n";
+  cout << o.get() << "\n";
+  cout << neg(o) << "\n";
+
+  return 0;
+}
